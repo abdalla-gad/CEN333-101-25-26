9b4e7a9ff9ba312186d7575b97f743f451e8c3ff
Abdalla Gad <abdallagad96@gmail.com>
2026-05-31 05:18:43 +0400

    full project

---
+++ src/environments/environments.ts
@@ -0,0 +1,15 @@
+export const environment ={
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCnVBT0z3Ylfn47jjo58P0FhbnXrsq1lRg",
+        authDomain: "vitaltrend-101-3a2df.firebaseapp.com",
+        projectId: "vitaltrend-101-3a2df",
+        storageBucket: "vitaltrend-101-3a2df.firebasestorage.app",
+        messagingSenderId: "913385925330",
+        appId: "1:913385925330:web:2e3da792bf8f7eb137db0b"
+      },
+
+    mapApiKey:"AIzaSyBkki9sczdUGOswgWix3JQZER3kb1vWQZU",
+    geminiApiKey:"AIzaSyBagJSgHWver991zw7cO3GPQit_WRfbNfw"
+}
+
